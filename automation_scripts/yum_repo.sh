26094d029021a96afc9933e9f2579a6e164fa8ae
fjemal <fjemal@users.noreply.github.com>
2018-05-12 17:30:20 -0700

    Update yum_repo.sh

--- automation_scripts/yum_repo.sh
+++ automation_scripts/yum_repo.sh
@@ -8,7 +8,7 @@ ip=$(gcloud compute instances list | grep yumrepo | awk '{print $4}') # will dyn
 
 echo "[fjemalrepo]
 
-name=fjemalrepo NTI-320 - $basearch
+name=fjemalrepo nti-320 - $basearch
 
 baseurl=http://$ip/centos/7/extras/x86_64/Packages/
 
@@ -16,12 +16,12 @@ enabled=1
 
 gpgcheck=0
 
-" >> /etc/yum.repos.d/NTI-320.repo                                  
+" >> /etc/yum.repos.d/nti-320.repo                                  
                                                                     # Now that the repo is added, list all repos and make sure
                                                                     # it shows up without error
 yum repolist
 
-isokay=`yum search "fjemalrepo NTI-320" | grep "fjemalrepo NTI-320"`
+isokay=`yum search "fjemalrepo nti-320" | grep "fjemalrepo NTI-320"`
 
 if [ -z "$isokay" ]; then
    echo "There's somthing wrong with your repo... check yum repolist to see if it shows up then try installing a package"
