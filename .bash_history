03a4a95c412042a24d92a3b827047324acb507a9
root <root@rpmbuild-server.c.devops-203203.internal>
2018-06-06 03:33:47 +0000

    first commit

---
+++ .bash_history
@@ -0,0 +1,47 @@
+sudo su
+ls
+gcloud autho login
+gcloud auth login
+sudo su
+su - fadil_jemal
+ls
+su - fadil.jemal
+su - Fadil.Jemal
+sudo su
+ls
+sudo su
+ls
+./gclodauto.sh 
+ls
+sudo su
+ls
+./gclodauto.sh 
+vim gclodauto.sh 
+sudo su
+ls
+sudo su
+ls
+vim gclodauto.sh 
+./gclodauto.sh 
+sudo su 
+ls
+vim gclodauto.sh 
+sudo su
+ls
+sudo su 
+ls
+./gcl
+./gclodauto.sh 
+sudo su
+sudo us
+sudosu
+sudo su
+ls
+sudo su
+suod su
+ls
+sudo su
+ls
+sudo su
+sudo su 
+sudo su
---
+++ rappar-gene.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# wrapper script for generate_config.sh
+
+#ip=$(gcloud compute instances list | grep yumrepo-a | awk '{print $5}') # will dynamically add ip address
+
+1=$(gcloud compute instances list | grep -v test-vm-a | awk '{print $1}') # prints a list of instance NAMES
+
+2=$(gcloud compute instances list | grep -v test-vm-a | awk '{print $5}') # prints a list of EXTERNAL_IPs
+
+myusername="FdilJemal"                           # username
+mynagiosserver="nagios2"                       # nagios server name
+#mycactiserver="cacti-a"                         # cacti server name
+myreposerver="rpm-3"                        # repo server
+mynagiosserverip="35.197.66.103"                # ip address of your nagios server
+
+generate_config.sh $1 $2              # script to generate nagios config $1 >> hostname, $2 >> host IP address
+
+#gcloud compute copy-files $1.cfg $myusername@$mynagiosserver:/etc/nagios/conf.d
+
+                                      # note: add user Jonathan to group nagios using 'usermod -a -G nagios Jonathan' on nagios server
+                                      # also chmod 770 /etc/nagios/conf.d
+
+
+# command to scp cfg file from instance to local directory
+gcloud compute scp builder-server:/home/fadil_jemal/builder-server.cfg /Users/Fadil/ --ssh-key-file=/Users/Jonathan/.ssh/google_compute_engine --zone us-west1-a
+
+# command to scp cfg file from local directory to nagios server
+#gcloud compute scp /Users/Jonathan/test-vm-a.cfg Jonathan@nagios-a:/etc/nagios/conf.d/ --ssh-key-file=/Users/Jonathan/.ssh/google_compute_engine --zone us-west1-a
+
+
+#gcloud compute ssh $myusername@$mynagiosserver \
+#"sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg" \
+#| grep "Things look okay - No serious problems" \
+
+gcloud compute ssh fadil_jemal@nagios2 --zone us-west1-a --command "sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg" | grep "Things look okay - No serious problems"
+
+if [[ $configstatus ]]; then
+   gcloud compute ssh $myusername@$mynagiosserver "sudo systemctl restart nagios"
+   echo "$1 has been added to nagios."
+else
+   echo "There was a problem with the nagios config, please log into $mynagiosserver and run /usr/sbin/nagios -v /etc/nagios/nagios.cfg to figure out where the problem is";
+   exit 1;
+fi
